d83cea54b4048f8716f994c656793e8442329324
anacarolinacavalheiro1-sys <anacarolinacavalheiro1@gmail.com>
2026-08-03 23:30:03 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,3 +73,8 @@ def set_webhook():
         return "Webhook configurado com sucesso!", 200
     return "Falha ao configurar Webhook.", 400
     
+if __name__ == "__main__":
+    import os
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=porta)
+    
